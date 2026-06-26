a835b89f948a54b05269d106fc2bf3ea89ade63d
EvgeshaXT <evgeshaxt@mail.ru>
2026-06-27 00:53:16 +0300

    v0.0.2.10.4 - Abstract class for main menu buttons

---
+++ MainMenuButtonBase.cs
@@ -0,0 +1,115 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace RhytmXT;
+
+internal abstract class MainMenuButtonBase : IContainsCursor
+{
+    internal Vector2 CLICKED_POSITION;
+    internal Vector2 HIDDEN_POSITION;
+    internal string _textureName;
+
+    Texture2D _texture;
+    Vector2 _position, newPosition, _origin;
+    float _scale, newScale;
+    readonly float scaleNormal, scaleMousehover;
+    Rectangle _area;
+    internal bool IsAppearing { get; set; }
+    internal bool IsDisappearing { get; set; }
+
+    protected MainMenuButtonBase()
+    {
+        CLICKED_POSITION = Vector2.Zero;
+        HIDDEN_POSITION = Vector2.Zero;
+
+        _position = Vector2.Zero;
+        newPosition = Vector2.Zero;
+
+        _scale = 0.55f;
+        newScale = _scale;
+        scaleNormal = _scale;
+        scaleMousehover = 0.6f;
+
+        IsAppearing = false;
+        IsDisappearing = false;
+    }
+
+    internal void Initialize()
+    {
+        _position = HIDDEN_POSITION;
+        newPosition = _position;
+    }
+
+    internal void LoadContent(ContentManager content)
+    {
+        _texture = content.Load<Texture2D>(_textureName);
+        _origin = new(0, _texture.Height / 2);
+    }
+
+    internal void Update(double deltaTime)
+    {
+        if (IsAppearing || IsDisappearing) AppearingAnimation(deltaTime);
+
+        Mousehover(deltaTime);
+    }
+
+    internal void Draw(SpriteBatch spriteBatch)
+    {
+        spriteBatch.Draw(_texture, _position, null, Color.White, 0, _origin, _scale, SpriteEffects.None, 0f);
+    }
+
+    public bool ContainsCursor()
+    {
+        int Width = (int)(_texture.Width * _scale);
+        int Height = (int)(_texture.Height * _scale);
+
+        _area = new((int)_position.X,
+                    (int)_position.Y - Height / 2,
+                    Width,
+                    Height);
+
+        return _area.Contains(MouseInputManager.MousePosition);
+    }
+
+    void Mousehover(double deltaTime)
+    {
+        if (ContainsCursor()) newScale = scaleMousehover;
+        else newScale = scaleNormal;
+
+        if (_scale != newScale)
+        {
+            float lerpFactor = 1 - (float)Math.Exp(-16f * deltaTime);
+
+            _scale += (newScale - _scale) * lerpFactor;
+
+            if (Math.Abs(_scale - newScale) < 0.001f) _scale = newScale;
+        }
+    }
+
+    void AppearingAnimation(double deltaTime)
+    {
+        UpdateDirectionForAnimation();
+
+        if (_position != newPosition)
+        {
+            float lerpFactor = 1 - (float)Math.Exp(-16f * deltaTime);
+            _position += (newPosition - _position) * lerpFactor;
+
+            if (Vector2.Distance(_position, newPosition) < 1f) _position = newPosition;
+        }
+    }
+
+    void UpdateDirectionForAnimation()
+    {
+        if (IsAppearing)
+        {
+            newPosition = CLICKED_POSITION;
+        }
+        else if (IsDisappearing)
+        {
+            newPosition = HIDDEN_POSITION;
+        }
+    }
+}
--- MainMenuMultiButton.cs
+++ MainMenuMultiButton.cs
@@ -1,106 +1,14 @@
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.Graphics;
-using System;
-
 namespace RhytmXT;
 
-internal class MainMenuMultiButton : IContainsCursor
+internal class MainMenuMultiButton : MainMenuButtonBase
 {
-    Texture2D _texture;
-    Vector2 _position, newPosition, _origin;
-    float _scale, newScale;
-    int screenWidth, screenHeight;
-    readonly float scaleNormal, scaleMousehover;
-    Rectangle _area;
-    internal bool IsAppearing { get; set; }
-    internal bool IsDisappearing { get; set; }
-    
     public MainMenuMultiButton(int screenWidth, int screenHeight)
     {
-        _position = new(screenWidth / 3f, screenHeight / 2f);
-        newPosition = _position;
+        CLICKED_POSITION = new(screenWidth / 2f + 25, screenHeight / 2f);
+        HIDDEN_POSITION = new(screenWidth / 3f, screenHeight / 2f);
 
-        _scale = 0.55f;
-        newScale = _scale;
-        scaleNormal = _scale;
-        scaleMousehover = 0.6f;
+        _textureName = "MainMenu/Multi";
 
-        this.screenWidth = screenWidth;
-        this.screenHeight = screenHeight;
-
-        IsAppearing = false;
-        IsDisappearing = false;
-    }
-
-    internal void LoadContent(ContentManager content)
-    {
-        _texture = content.Load<Texture2D>("MainMenu/Multi");
-        _origin = new(0, _texture.Height / 2);
-    }
-
-    internal void Update(double deltaTime)
-    {
-        if (IsAppearing || IsDisappearing) mainMenuMulti_AppearingAnimation(deltaTime);
-
-        mainMenuMulti_Mousehover(deltaTime);
-    }
-
-    internal void Draw(SpriteBatch spriteBatch)
-    {
-        spriteBatch.Draw(_texture, _position, null, Color.White, 0, _origin, _scale, SpriteEffects.None, 0f);
-    }
-
-    public bool ContainsCursor()
-    {
-        int Width = (int)(_texture.Width * _scale);
-        int Height = (int)(_texture.Height * _scale);
-
-        _area = new((int)_position.X,
-                    (int)_position.Y - Height / 2,
-                    Width,
-                    Height);
-
-        return _area.Contains(MouseInputManager.MousePosition);
-    }
-
-    void mainMenuMulti_Mousehover(double deltaTime)
-    {
-        if (ContainsCursor()) newScale = scaleMousehover;
-        else newScale = scaleNormal;
-
-        if (_scale != newScale)
-        {
-            float lerpFactor = 1 - (float)Math.Exp(-16f * deltaTime);
-
-            _scale += (newScale - _scale) * lerpFactor;
-
-            if (Math.Abs(_scale - newScale) < 0.001f) _scale = newScale;
-        }
-    }
-
-    void mainMenuMulti_AppearingAnimation(double deltaTime)
-    {
-        UpdateDirectionForAnimation();
-
-        if (_position != newPosition)
-        {
-            float lerpFactor = 1 - (float)Math.Exp(-16f * deltaTime);
-            _position += (newPosition - _position) * lerpFactor;
-
-            if (Vector2.Distance(_position, newPosition) < 1f) _position = newPosition;
-        }
-    }
-
-    void UpdateDirectionForAnimation()
-    {
-        if (IsAppearing)
-        {
-            newPosition = new(screenWidth / 2f + 25f, screenHeight / 2f);
-        }
-        else if (IsDisappearing)
-        {
-            newPosition = new(screenWidth / 3f, screenHeight / 2f);
-        }
+        Initialize();
     }
 }
